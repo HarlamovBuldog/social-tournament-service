e8c024f41963a8ff65fda7c489582a86f90910a3
Vladwoode <vladwoode@gmail.com>
2019-08-05 21:21:05 +0300

    Error handling and config rework. Add validate method.
    log.Printf with return were changed to log.Fatalf.
    Same functionality but shorter.
    Add Validate method for config struct.
    Reason is to check all fields right after successful parsing config file.
    Config struct fields and config.yml fields were renamed for better understanding.
    defer func was created for handling mongodb client.Disconnect() errors.

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/pkg/errors"
 
 	"github.com/HarlamovBuldog/social-tournament-service/pkg/server"
 	"github.com/HarlamovBuldog/social-tournament-service/pkg/storage"
@@ -15,47 +18,64 @@ import (
 )
 
 type config struct {
-	ConnStr string `yaml:"connstr"`
-	Port    string `yaml:"port"`
-	DBName  string `yaml:"dbname"`
+	ConnStr    string `yaml:"conn_str"`
+	ServerPort string `yaml:"server_port"`
+	DBName     string `yaml:"db_name"`
+}
+
+func (conf *config) Validate() error {
+	if conf.ConnStr == "" {
+		return errors.New("connection string is not provided")
+	}
+	if conf.DBName == "" {
+		return errors.New("database name is not provided")
+	}
+	if conf.ServerPort == "" {
+		return errors.New("server port is not provided")
+	} else if _, err := strconv.ParseUint(conf.ServerPort, 0, 64); err != nil {
+		return errors.Wrap(err, "bad server port provided")
+	}
+	return nil
 }
 
 func main() {
 	yamlConfigFile, err := ioutil.ReadFile("config.yaml")
 	if err != nil {
-		log.Printf("error opening cofiguration yaml file: %v\n", err)
-		return
+		log.Fatalf("error opening cofiguration yaml file: %v\n", err)
 	}
+
 	var conf config
 	err = yaml.Unmarshal(yamlConfigFile, &conf)
 	if err != nil {
-		log.Printf("error unmarshaling yaml configuration file: %v\n", err)
-		return
+		log.Fatalf("error unmarshaling yaml configuration file: %v\n", err)
 	}
-	if conf.ConnStr == "" {
-		log.Println("error in yaml configuration file: connection string is not provided")
-		return
+
+	if err = conf.Validate(); err != nil {
+		log.Fatalf("error validating config file: %v", err)
 	}
+
 	clientOptions := options.Client().ApplyURI(conf.ConnStr)
-
 	client, err := mongo.Connect(context.TODO(), clientOptions)
-	defer client.Disconnect(context.TODO())
-
 	if err != nil {
-		log.Printf("error connecting to mongo db: %v", err)
+		log.Fatalf("error connecting to mongo db: %v", err)
 	}
+
+	defer func() {
+		err := client.Disconnect(context.TODO())
+		if err != nil {
+			log.Fatalf("error disconnecting from mongo db: %v", err)
+		}
+	}()
+
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		log.Printf("error connecting to mongo db: %v", err)
+		log.Fatalf("error connecting to mongo db: %v", err)
 	}
 
 	log.Println("Connected to MongoDB!")
 
-	db := storage.CreateNew(client.Database("sts"))
+	db := storage.CreateNew(client.Database(conf.DBName))
 	s := server.NewServer(db)
-	if conf.Port == "" {
-		log.Println("error in yaml configuration file: port is not provided")
-		return
-	}
-	log.Fatal(http.ListenAndServe("localhost"+conf.Port, s))
+
+	log.Fatal(http.ListenAndServe("localhost:"+conf.ServerPort, s))
 }
