5282070d38ce8cc6579d89e24257ae8dad66d4ea
Vladwoode <vladwoode@gmail.com>
2019-08-13 20:09:42 +0300

    Change error handling in user_test.go file.
    Change "if" with following "t.Fatal" statements to require.NoError() func.
    Here we are use maximum functionality of testify/require package to handle nil errors.
    Also problem was that t.Fatal called FailNow() which caused too early exit from test func.

--- internal/storage/user_test.go
+++ internal/storage/user_test.go
@@ -5,27 +5,23 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func TestAddUser(t *testing.T) {
 	userIDExpected, err := db.AddUser(context.TODO(), "gennadiy")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "AddUser func should return nil error")
 
 	actualUser, err := db.GetUser(context.TODO(), userIDExpected)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "GetUser func should return nil error")
+
 	assert := assert.New(t)
 	assert.Equal(userIDExpected, actualUser.ID.Hex(), "The two IDs should be the same.")
 
 	userIDExpected2, err := primitive.ObjectIDFromHex(userIDExpected)
-	if err != nil {
-		t.Fatalf("error converting to primitive.ObjectID: %v", err)
-	}
+	require.NoError(t, err, "ObjectIDFromHex func should return nil error")
 
 	assert.Equal(&User{ID: userIDExpected2, Name: "gennadiy"}, actualUser, "The two users should be the same.")
 
@@ -34,21 +30,16 @@ func TestAddUser(t *testing.T) {
 
 func TestGetUser(t *testing.T) {
 	userIDExpected, err := db.AddUser(context.TODO(), "Vasya")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "AddUser func should return nil error")
 
 	actualUser, err := db.GetUser(context.TODO(), userIDExpected)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "GetUser func should return nil error")
+
 	assert := assert.New(t)
 	assert.Equal(userIDExpected, actualUser.ID.Hex(), "The two IDs should be the same.")
 
 	userIDExpected2, err := primitive.ObjectIDFromHex(userIDExpected)
-	if err != nil {
-		t.Fatalf("error converting to primitive.ObjectID: %v", err)
-	}
+	require.NoError(t, err, "ObjectIDFromHex func should return nil error")
 
 	assert.Equal(&User{ID: userIDExpected2, Name: "Vasya"}, actualUser, "The two users should be the same.")
 
@@ -67,13 +58,10 @@ func TestGetUser(t *testing.T) {
 
 func TestDeleteUser(t *testing.T) {
 	userIDExpected, err := db.AddUser(context.TODO(), "Vasya")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "AddUser func should return nil error")
 
-	if err = db.DeleteUser(context.TODO(), userIDExpected); err != nil {
-		t.Fatal(err)
-	}
+	err = db.DeleteUser(context.TODO(), userIDExpected)
+	require.NoError(t, err, "DeleteUser func should return nil error")
 
 	badUserID := "safasf2412"
 	err = db.DeleteUser(context.TODO(), badUserID)
@@ -105,28 +93,19 @@ func TestTakeUserBalance(t *testing.T) {
 		"The error should contain text")
 
 	addedUserID, err := db.AddUser(context.TODO(), "Vasya")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "AddUser func should return nil error")
 
 	err = db.TakeUserBalance(context.TODO(), addedUserID, amount)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "TakeUserBalance func should return nil error")
 
 	addedUser, err := db.GetUser(context.TODO(), addedUserID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "GetUser func should return nil error")
 
 	addedUserObjectID, err := primitive.ObjectIDFromHex(addedUserID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "ObjectIDFromHex func should return nil error")
 
 	assert.Equal(&User{ID: addedUserObjectID, Name: "Vasya", Balance: -100.0}, addedUser,
 		"The two users should be the same.")
-
 }
 
 func TestFundUserBalance(t *testing.T) {
@@ -146,24 +125,16 @@ func TestFundUserBalance(t *testing.T) {
 		"The error should contain text")
 
 	addedUserID, err := db.AddUser(context.TODO(), "Vasya")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "AddUser func should return nil error")
 
 	err = db.FundUserBalance(context.TODO(), addedUserID, amount)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "FundUserBalance func should return nil error")
 
 	addedUser, err := db.GetUser(context.TODO(), addedUserID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "GetUser func should return nil error")
 
 	addedUserObjectID, err := primitive.ObjectIDFromHex(addedUserID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err, "ObjectIDFromHex func should return nil error")
 
 	assert.Equal(&User{ID: addedUserObjectID, Name: "Vasya", Balance: 100.0}, addedUser,
 		"The two users should be the same.")
