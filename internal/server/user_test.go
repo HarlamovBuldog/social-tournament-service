56893e1a842d5e8d428dddd3ed878eca1fe6426d
Vladwoode <vladwoode@gmail.com>
2019-08-19 21:46:17 +0300

    Add tests for GetUserInfo and RemoveUser handlers
    Request variables are set with help of SetURLVars func of gorilla/mux lib.
    Cause mux.Vars can't read URL vars from httptest.NewRequest.

--- internal/server/user_test.go
+++ internal/server/user_test.go
@@ -8,8 +8,10 @@ import (
 	"testing"
 
 	gomock "github.com/golang/mock/gomock"
+	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/HarlamovBuldog/social-tournament-service/internal/storage"
 )
@@ -78,6 +80,124 @@ func TestCreateNewUser_Bad_Req(t *testing.T) {
 
 	s := NewServer(mock)
 	s.createNewUser(w, req)
+
+	actualCode := w.Result().StatusCode
+	require.Equal(t, http.StatusBadRequest, actualCode, "The two http codes should be the same")
+}
+
+func TestGetUserInfo_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := storage.NewMockService(ctrl)
+	expectedUserID := primitive.NewObjectID()
+	expectedUser := &storage.User{ID: expectedUserID, Name: "Gennadiy", Balance: 0}
+	mock.EXPECT().GetUser(gomock.Any(), gomock.Eq(expectedUserID.Hex())).Times(1).Return(expectedUser, nil)
+
+	req := httptest.NewRequest("GET", "/user/", nil)
+	req = mux.SetURLVars(req, map[string]string{"id": expectedUserID.Hex()})
+	w := httptest.NewRecorder()
+
+	s := NewServer(mock)
+	s.getUserInfo(w, req)
+
+	actualCode := w.Result().StatusCode
+	require := require.New(t)
+	require.Equal(http.StatusOK, actualCode, "The two http codes should be the same")
+
+	var actualUser storage.User
+	err := json.NewDecoder(w.Result().Body).Decode(&actualUser)
+	require.NoError(err)
+	require.Equal(*expectedUser, actualUser, "The two objects shoud be the same")
+}
+
+func TestGetUserInfo_DB_Fail(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := storage.NewMockService(ctrl)
+	expectedUserID := primitive.NewObjectID()
+	mock.EXPECT().GetUser(gomock.Any(), gomock.Eq(expectedUserID.Hex())).
+		Times(1).Return(nil, errors.New("get doc from collection"))
+
+	req := httptest.NewRequest("GET", "/user", nil)
+	req = mux.SetURLVars(req, map[string]string{"id": expectedUserID.Hex()})
+	w := httptest.NewRecorder()
+
+	s := NewServer(mock)
+	s.getUserInfo(w, req)
+
+	actualCode := w.Result().StatusCode
+	require.Equal(t, http.StatusInternalServerError, actualCode, "The two http codes should be the same")
+}
+
+func TestGetUserInfo_Bad_Req(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := storage.NewMockService(ctrl)
+	mock.EXPECT().GetUser(gomock.Any(), gomock.Any()).Times(0)
+
+	req := httptest.NewRequest("GET", "/user", nil)
+	w := httptest.NewRecorder()
+
+	s := NewServer(mock)
+	s.getUserInfo(w, req)
+
+	actualCode := w.Result().StatusCode
+	require.Equal(t, http.StatusBadRequest, actualCode, "The two http codes should be the same")
+}
+
+func TestRemoveUser_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := storage.NewMockService(ctrl)
+	userID := primitive.NewObjectID()
+	mock.EXPECT().DeleteUser(gomock.Any(), gomock.Eq(userID.Hex())).Times(1).Return(nil)
+
+	req := httptest.NewRequest("DELETE", "/user/", nil)
+	req = mux.SetURLVars(req, map[string]string{"id": userID.Hex()})
+	w := httptest.NewRecorder()
+
+	s := NewServer(mock)
+	s.removeUser(w, req)
+
+	actualCode := w.Result().StatusCode
+	require.Equal(t, http.StatusOK, actualCode, "The two http codes should be the same")
+}
+
+func TestRemoveUser_DB_Fail(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := storage.NewMockService(ctrl)
+	userID := primitive.NewObjectID()
+	mock.EXPECT().DeleteUser(gomock.Any(), gomock.Eq(userID.Hex())).Times(1).Return(errors.New("delete doc from collection"))
+
+	req := httptest.NewRequest("DELETE", "/user/", nil)
+	req = mux.SetURLVars(req, map[string]string{"id": userID.Hex()})
+	w := httptest.NewRecorder()
+
+	s := NewServer(mock)
+	s.removeUser(w, req)
+
+	actualCode := w.Result().StatusCode
+	require.Equal(t, http.StatusInternalServerError, actualCode, "The two http codes should be the same")
+}
+
+func TestRemoveUser_Bad_Req(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := storage.NewMockService(ctrl)
+	mock.EXPECT().DeleteUser(gomock.Any(), gomock.Any()).Times(0)
+
+	req := httptest.NewRequest("DELETE", "/user", nil)
+	w := httptest.NewRecorder()
+
+	s := NewServer(mock)
+	s.removeUser(w, req)
 
 	actualCode := w.Result().StatusCode
 	require.Equal(t, http.StatusBadRequest, actualCode, "The two http codes should be the same")
